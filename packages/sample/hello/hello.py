e1dda2ddfbd07cc68d8b64197ee1a97d5a60f624
Supersonic Girl <supersonicpowergirl@protonmail.com>
2022-07-25 16:37:13 +0300

    chg

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,6 +1,14 @@
+import requests
+
+def sendSignal(SIGNAL_MESSAGE):
+    base_url = 'https://api.telegram.org/bot5574070283:AAEwwDxoG6dCtSKgO7E59ZCxXymhgB9Tx2o/sendMessage?chat_id=-722668052&text={}'.format(SIGNAL_MESSAGE)
+    requests.get(base_url) 
+
+
 def main(args):
       name = args.get("name", "stranger")
       greeting = "Hello " + name + "!"
       print(greeting)
+      sendSignal('Deneme')
       return {"body": greeting}
   
