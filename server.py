5c07917fe95529dca165a4d25255d2377bd74fc7
Supersonic Girl <supersonicpowergirl@protonmail.com>
2022-07-25 16:22:44 +0300

    chg

--- server.py
+++ server.py
@@ -1,19 +1,9 @@
-import os
-import http.server
-import socketserver
+import requests
 
-from http import HTTPStatus
-
-
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+def sendSignal(SIGNAL_MESSAGE):
+    base_url = 'https://api.telegram.org/bot5574070283:AAEwwDxoG6dCtSKgO7E59ZCxXymhgB9Tx2o/sendMessage?chat_id=-722668052&text={}'.format(SIGNAL_MESSAGE)
+    requests.get(base_url) 
+    print(SIGNAL_MESSAGE)
 
 
-port = int(os.getenv('PORT', 80))
-print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+sendSignal('Deneme')
