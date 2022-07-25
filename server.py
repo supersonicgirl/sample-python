ec5cd0ad56e51d102c01138060fdd060ecdf377f
Supersonic Girl <supersonicpowergirl@protonmail.com>
2022-07-25 16:30:27 +0300

    chg

--- server.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def sendSignal(SIGNAL_MESSAGE):
-    base_url = 'https://api.telegram.org/bot5574070283:AAEwwDxoG6dCtSKgO7E59ZCxXymhgB9Tx2o/sendMessage?chat_id=-722668052&text={}'.format(SIGNAL_MESSAGE)
-    requests.get(base_url) 
-    print(SIGNAL_MESSAGE)
-
-
-sendSignal('Deneme')
